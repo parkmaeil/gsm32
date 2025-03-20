878e2386e923dda4e43ad413d292c30085b3cc20
bitcocom <bitcocom@empas.com>
2025-03-20 11:10:47 +0900

    spring

--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Board;
 import com.example.demo.repository.BoardRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +26,13 @@ public class BoardService { // new BoardService();
 
     public Optional<Board> findById(Long id){
        return boardRepository.findById(id); // where id=
+    }
+
+    public Board save(Long id, Board reqBoard){
+        return boardRepository.findById(id).map(board->{
+            board.setTitle(reqBoard.getTitle());
+            board.setContent(reqBoard.getContent());
+            return boardRepository.save(board);
+        }).orElseThrow(() -> new EntityNotFoundException("Board not found with id: " + id));
     }
 }
