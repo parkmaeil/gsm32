2ab9cfea8ee4bb0d6782982f1c25e7cc1edc952c
bitcocom <bitcocom@empas.com>
2025-03-27 12:22:30 +0900

    spring

---
+++ src/main/java/com/example/demo/entity/ReviewDTO.java
@@ -0,0 +1,19 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewDTO {
+    private Long id;
+    private int cost;
+    private String content;
+    private LocalDateTime createdAt;
+}
