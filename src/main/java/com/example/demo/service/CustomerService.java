1cdac5f99ea639b86d6cb69b61a836daa9cd1390
bitcocom <bitcocom@empas.com>
2025-04-03 11:52:20 +0900

    spring

--- src/main/java/com/example/demo/service/CustomerService.java
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -1,9 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Book;
+import com.example.demo.entity.Customer;
+import com.example.demo.payload.BookInfoDTO;
+import com.example.demo.payload.CartDTO;
+import com.example.demo.payload.CustomerCartDTO;
 import com.example.demo.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,9 +20,19 @@ public class CustomerService {
 
     @Transactional(readOnly = true)
     public List<CustomerCartDTO> getCustomersWithCarts(){
-
-
-        return null;
+        List<Customer> customers=customerRepository.findAllWithCartsAndBooks();
+        // Book->BookInfoDTO
+        // Cart->CartDTO
+        // Customer->CustomerCartDTO
+        return customers.stream().map(customer->{
+            List<CartDTO> cartDTOS=customer.getCarts().stream().map(cart->{
+                Book book=cart.getBook();
+                BookInfoDTO bookInfoDTO=new BookInfoDTO(book.getTitle(), book.getAuthor(), book.getPrice());
+                return new CartDTO(cart.getQuantity(), cart.getCartDate(), bookInfoDTO);
+            }).toList();
+          return new CustomerCartDTO(customer.getUsername(), customer.getName(), customer.getAge(), cartDTOS);
+        }).toList();
     }
 
 }
+
