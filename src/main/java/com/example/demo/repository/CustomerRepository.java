719cb8ef51b7381c50ea631d888a34ff3a26c788
bitcocom <bitcocom@empas.com>
2025-04-03 11:03:13 +0900

    spring

---
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+  // fetch join
+   @Query("""
+           SELECT DISTINCT c FROM Customer c
+           LEFT JOIN FETCH c.carts ct
+           LEFT JOIN FETCH ct.book
+           """)
+   public List<Customer> findAllWithCartsAndBooks();
+
+}
---
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+
+}
